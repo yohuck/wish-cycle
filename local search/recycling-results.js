ff615a4876ff78a3e0009b38a81926bdb219f0c0
Eric Lake <99624222+yohuck@users.noreply.github.com>
2022-06-06 19:07:21 -0600

    Revert "added 5th page and google search by location"

--- local search/recycling-results.js
+++ local search/recycling-results.js
@@ -3,14 +3,9 @@ var location1 = document.getElementById('location1')
 var generalBtn = document.getElementById('general')
 var electronicsBtn = document.getElementById('electronics')
 var hazardBtn = document.getElementById('hazard')
-let fifth = document.getElementById('fifth')
-let fourth = document.getElementById('fourth')
-
 
 var generalSearch = () =>{
     const successCallback = (position) => {
-        fourth.setAttribute('style','transform: translate(-100%)');
-        fifth.setAttribute('style','transform: translate(0%)')
         console.log(position);
         var latitude = position.coords.latitude
         var longitude = position.coords.longitude
@@ -22,12 +17,12 @@ var generalSearch = () =>{
                 type: 'get',
             }) .done(function (data){
                 console.log(data)
-                // location1.textContent = 'General Recycling for '+ data[0].name + ', ' + data[0].state
-                let city = data[0].name
+                location1.textContent = 'General Recycling for '+ data[0].name + ', ' + data[0].state
+                city = data[0].name
 
                 let result1 = '';
                 // commented out URL
-                let url2 = 'https://api.valueserp.com/search?api_key=A194679C5A1340D5986D72FAE3E67B62&q=recycling&location='+ city+'&gl=us&cr=us&lr=lang_en&num=3'
+                // let url2 = 'https://api.valueserp.com/search?api_key=A194679C5A1340D5986D72FAE3E67B62&q=recycling&location='+ city+'&gl=us&cr=us&lr=lang_en&num=3'
 
                 $.ajax({
                 url: url2,
@@ -35,11 +30,11 @@ var generalSearch = () =>{
                 }).done(function(data){
                     console.log(data)
                     data.local_results.forEach(result =>{
-                    result1 = `<h1 class="has-text-white title is-3">${result.title}</h1>
-                        <p class="has-text-white">${result.extensions[4]}</p>
-                        <a class="has-text-white">${result.link}</a><br>`
+                    result1 = `<h1 class="title is-3">${result.title}</h1>
+                        <p>Phone Number: ${result.extensions[4]}</p>
+                        <a>${result.link}</a><br>`
 
-                    $("#loader").replaceWith(result1)
+                    $("#result").append(result1)
                 })     
             })  
             })
@@ -69,7 +64,7 @@ var electronicSearch = () =>{
                 console.log(data)
                 $('#hidden').removeClass('hide')
                 location1.textContent = 'Electronics Recycling for '+ data[0].name + ', ' + data[0].state
-                let city = data[0].name
+                city = data[0].name
     
                 let result1 = '';
                 // Commented out URL
@@ -84,7 +79,7 @@ var electronicSearch = () =>{
                     result1 = `<h1 class="title is-3">${result.title}</h1>
                         <p>${result.extensions[4]}</p>
                         <a>${result.link}</a><br>`
-                    $("#result2").append(result1)
+                    $("#result").append(result1)
                 })     
             })  
             })
@@ -114,7 +109,7 @@ var hazardSearch = () =>{
                 console.log(data)
                 $('#hidden').removeClass('hide')
                 location1.textContent = 'Hazardous Recycling for '+ data[0].name + ', ' + data[0].state
-                let city = data[0].name
+                city = data[0].name
     
                 let result1 = '';
                 // Commented out URL
--- ui.js
+++ ui.js
@@ -1,36 +1,36 @@
-let landing = document.getElementById('landing');
-let submiter = document.getElementById('searchByPicture');
-let second = document.getElementById('second');
-let cards = document.getElementsByClassName('pageTwo');
-let pageThree = document.getElementsByClassName('pageThree');
-let pizza = document.getElementById('pizza')
+// let landing = document.getElementById('landing');
+// let submiter = document.getElementById('searchByPicture');
+// let second = document.getElementById('second');
+// let cards = document.getElementsByClassName('pageTwo');
+// let pageThree = document.getElementsByClassName('pageThree');
+// let pizza = document.getElementById('pizza')
 
 
 
-submiter.addEventListener('click',function(){
-    console.log('hello')
-    landing.setAttribute('style','transform: translate(-100%)');
-    second.setAttribute('style','transform: translate(0%)')
-})
+// submiter.addEventListener('click',function(){
+//     console.log('hello')
+//     landing.setAttribute('style','transform: translate(-100%)');
+//     second.setAttribute('style','transform: translate(0%)')
+// })
 
 
 
-for (let i = 0; i < cards.length; i++){
-    cards[i].addEventListener('click',function(){
-        second.setAttribute('style','transform: translate(-100%');
-        third.setAttribute('style','transform: translate(0%');
-    })
-}
+// for (let i = 0; i < cards.length; i++){
+//     cards[i].addEventListener('click',function(){
+//         second.setAttribute('style','transform: translate(-100%');
+//         third.setAttribute('style','transform: translate(0%');
+//     })
+// }
 
-for (let i = 0; i < pageThree.length; i++){
-    pageThree[i].addEventListener('click',function(event){
-        console.log(this.id);
-        console.log(event)
-        third.setAttribute('style','transform: translate(-100%');
-        fourth.setAttribute('style','transform: translate(0%');
-    })
-}
+// for (let i = 0; i < pageThree.length; i++){
+//     pageThree[i].addEventListener('click',function(event){
+//         console.log(this.id);
+//         console.log(event)
+//         third.setAttribute('style','transform: translate(-100%');
+//         fourth.setAttribute('style','transform: translate(0%');
+//     })
+// }
 
-pizza.addEventListener('click', function(){
-    console.log(this.id)
-})
+// pizza.addEventListener('click', function(){
+//     console.log(this.id)
+// })
